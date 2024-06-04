841847fb9d756abbbd316b88d9b006aaaf91a8e9
germandbl <gdb.lopez@outlook.com>
2024-06-03 18:41:59 -0600

    Tema aplicado mediante el constructor
    Luego de añadir un parámetro requerido al constructor de AppTheme, se aplicó el tema de color azul en el main.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(0),
+      theme: AppTheme(selectedColor: 0).getTheme(),
     );
   }
 }
