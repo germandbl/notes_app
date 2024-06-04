b676e7b8ba934f0cf739a4c2053c37bac97a4576
germandbl <gdb.lopez@outlook.com>
2024-06-03 21:25:04 -0600

    Layout base terminado
    Se implementó un SingleChildScrollView para que sea scrollable la pantalla al haber muchas cards, también se agregó el botón flotante que permitirá crear nuevas notas en el futuro.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/presentation/widgets/custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mis notas"),
+        centerTitle: false,
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            children: [
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {}, child: const Icon(Icons.add_rounded)),
+    );
   }
 }
+
