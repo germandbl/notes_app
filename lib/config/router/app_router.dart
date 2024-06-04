525ab8b67bd199e6d33ab55f335710245a781a8c
germandbl <gdb.lopez@outlook.com>
2024-06-03 21:36:12 -0600

    Ruta para ver nota configurada

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,9 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:notes_app/presentation/screens/home_screen.dart';
+import 'package:notes_app/presentation/screens/note_screen.dart';
 
-final appRouter = GoRouter(
-  initialLocation: "/",
-  routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-  ]
-);
+final appRouter = GoRouter(initialLocation: "/", routes: [
+  GoRoute(path: '/', builder: (context, state) => const HomeScreen(), routes: [
+    GoRoute(path: 'note/:id', builder: (context, state) => const NoteScreen())
+  ]),
+]);
