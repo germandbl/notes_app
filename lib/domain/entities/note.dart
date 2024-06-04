e200dccf973bb9c03748f24a1c980bc6ee404ea9
germandbl <gdb.lopez@outlook.com>
2024-06-04 15:33:03 -0600

    Campos priority y state agregados

--- lib/domain/entities/note.dart
+++ lib/domain/entities/note.dart
@@ -2,6 +2,13 @@ class Note {
   final int noteId;
   final String title;
   final String description;
+  final String priority;
+  final String state;
 
-  Note({required this.noteId, required this.title, required this.description});
+  Note(
+      {required this.noteId,
+      required this.title,
+      required this.description,
+      required this.priority,
+      required this.state});
 }
