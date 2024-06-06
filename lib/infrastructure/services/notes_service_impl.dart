3efc73affec7cfab01d75566d029a42f0cb8404b
germandbl <gdb.lopez@outlook.com>
2024-06-06 17:34:00 -0600

    Implementacion base creada

---
+++ lib/infrastructure/services/notes_service_impl.dart
@@ -0,0 +1,17 @@
+import 'package:notes_app/domain/entities/note.dart';
+import 'package:notes_app/domain/services/notes_service.dart';
+
+class NotesServiceImpl extends NotesService {
+  @override
+  Future<Note> getNoteById() {
+    // TODO: implement getNoteById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Note>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
+
+}
