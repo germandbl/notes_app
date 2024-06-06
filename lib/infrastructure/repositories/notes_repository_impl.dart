535263b16aeeb03d950017f2118757a92e8b4ab2
germandbl <gdb.lopez@outlook.com>
2024-06-06 17:32:54 -0600

    Implementacion base

---
+++ lib/infrastructure/repositories/notes_repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:notes_app/domain/entities/note.dart';
+import 'package:notes_app/domain/repositories/notes_repository.dart';
+
+class NotesRepositoryImpl extends NotesRepository {
+  @override
+  Future<Note> getNoteById() {
+    // TODO: implement getNoteById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Note>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
+
+}
